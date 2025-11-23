01fe83ff1c38a4b5ff41aeeaecc0282595702a9b
DGorenflo <gorenflodavid@gmail.com>
2025-11-23 12:31:27 +0100

    reoved jitter

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import requests
 import json
 from datetime import datetime
-import os
 import pytz
 
 API_URL = "https://www.ai-fitness.de/connect/v1/studio/1360175120/utilization"
 LOG_FILE = "fitness_history.jsonl"
 
 def fetch_and_save():
+
     try:
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
